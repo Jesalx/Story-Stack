1e243cf3794ead922f2f5542ee1e6c5180504a0b
Jesal Patel <jesalpatelx@gmail.com>
2022-04-04 16:54:47 -0400

    Updated script for email use

--- static/script.js
+++ static/script.js
@@ -1,9 +1,9 @@
 function meetsLoginConditions() {
-    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
     let err_msg = document.getElementById("messages");
 
-    if (username.length === 0 || password.length === 0) {
+    if (email.length === 0 || password.length === 0) {
         err_msg.innerText = "Please fill in all fields.";
         return false;
     }
@@ -12,18 +12,25 @@ function meetsLoginConditions() {
 }
 
 function meetsSignupConditions() {
+    let email = document.getElementById("email").value;
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     let repassword = document.getElementById("repassword").value;
     let err_msg = document.getElementById("messages");
 
-    if (username.length === 0 || password.length === 0 || repassword.length === 0) {
+    if (email.length === 0 || username.length === 0 || password.length === 0 || repassword.length === 0) {
         err_msg.innerText = "Please fill in all fields.";
         return false;
     }
 
     // How to use regular expressions in Javascript from:
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+    const email_re = /^[0-9a-zA-Z]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/;
+    if (!email.match(email_re)) {
+        err_msg.innerText = "Please provie a valid email.";
+        return false;
+    }
+
     const username_re = /^[0-9a-zA-Z]+$/;
     if (!username.match(username_re)) {
         err_msg.innerText = "Username may only contain alphanumeric characters.";
