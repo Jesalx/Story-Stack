7d050aa3da4916c767404dc365bc51fdca7cca08
Jesal Patel <jesalpatelx@gmail.com>
2022-04-06 17:49:03 -0400

    Added testing for story.py

---
+++ test_story.py
@@ -0,0 +1,44 @@
+import unittest
+from story import extract_tags
+
+
+class ExtractTagsTests(unittest.TestCase):
+    def test_regular_string(self):
+        test_string = "tag1, tag2, tag3"
+        expected_output = set(["tag1", "tag2", "tag3"])
+        actual_output = extract_tags(test_string)
+        self.assertEqual(expected_output, actual_output)
+
+    def test_empty_string(self):
+        test_string = ""
+        expected_output = set([])
+        actual_output = extract_tags(test_string)
+        self.assertEqual(expected_output, actual_output)
+
+    def test_whitespace_string(self):
+        test_string = "     "
+        expected_output = set([])
+        actual_output = extract_tags(test_string)
+        self.assertEqual(expected_output, actual_output)
+
+    def test_short_string(self):
+        test_string = "tag1"
+        expected_output = set(["tag1"])
+        actual_output = extract_tags(test_string)
+        self.assertEqual(expected_output, actual_output)
+
+    def test_weird_string(self):
+        test_string = ",,,,,,,tag1,,,,,,,,,@@@@,,,,,,tag2"
+        expected_output = set(["tag1", "tag2", "@@@@"])
+        actual_output = extract_tags(test_string)
+        self.assertEqual(expected_output, actual_output)
+
+    def test_comma_string(self):
+        test_string = ",,,,,,,,,,,,,"
+        expected_output = set([])
+        actual_output = extract_tags(test_string)
+        self.assertEqual(expected_output, actual_output)
+
+
+if __name__ == "__main__":
+    unittest.main()
